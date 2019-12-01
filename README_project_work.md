10f48758435bfe148a60056c4a9abb48a74adc8f
Miorini Iacopo <Iacopo.Miorini@nttdata.com>
2019-12-01 19:07:20 +0100

    Create mock data and readme, add images

---
+++ README_project_work.md
@@ -0,0 +1,18 @@
+
+## Grafiche
+
+Le grafiche sono reperibili da questo [link](https://zpl.io/Vxvl4lR) con le credenziali:
+
+>**username**: tag_ntt_project_work <br>
+>**password**: zeplin123!
+
+## Servizi
+
+I servizi REST disponibili sono:
+
+- Store =>
+- Steps =>
+- Tree =>
+
+Per qualsiasi dubbio o domande sono a disposizione
+
---
+++ src/constants/steps.js
@@ -0,0 +1,93 @@
+
+export default [
+    {
+        question: 'Che tipo di attività devi svolgere?',
+        category: 'Attività',
+        answers: [
+            {
+                id: 100,
+                title: 'Tenere in ordine il mio giardino',
+                description: 'Falciatura ordinaria del prato, rifinitura aiuole',
+                image: ''
+            },
+            {
+                id: 101,
+                title: 'Pulire e rinnovare il giardino',
+                description: 'Presenza di arbusti, erbe alte e spesse, piante infestanti da rimuovere',
+                image: ''
+            }
+        ]
+    },
+    {
+        question: 'Sono presenti piante o ostacoli?',
+        category: 'Ostacoli',
+        answers: [
+            {
+                id: 102,
+                title: 'Sono presenti aiuole e ostacoli di piccole dimensioni',
+                description: null,
+                image: null
+            },
+            {
+                id: 103,
+                title: 'Sono presenti arbusti, alberi e ostacoli di grandi dimensioni',
+                description: null,
+                image: null
+            },
+            {
+                id: 104,
+                title: 'No, non son presenti piante e ostacoli',
+                description: null,
+                image: null
+            }
+        ]
+    },
+    {
+        question: 'Ci sono pendenze o affossamenti?',
+        category: 'Pendenza',
+        answers: [
+            {
+                id: 105,
+                title: 'Sì, il mio terreno è pendente o presenta degli affossamenti',
+                description: null,
+                image: null
+            },
+            {
+                id: 106,
+                title: 'No, ho un terreno piano',
+                description: null,
+                image: null
+            }
+        ]
+    },
+    {
+        question: 'Che dimensioni ha il tuo terreno?',
+        category: 'Dimensioni',
+        answers: [
+            {
+                id: 107,
+                title: 'Fino a 100 m2',
+                description: null,
+                image: null
+            },
+            {
+                id: 108,
+                title: 'Da 100 a 500 m2',
+                description: null,
+                image: null
+            },
+            {
+                id: 109,
+                title: 'Da 500 a 1000 m2',
+                description: null,
+                image: null
+            },
+            {
+                id: 110,
+                title: 'Da 1000 a 2000 m2',
+                description: null,
+                image: null
+            }
+        ]
+    }
+];
